769c4af4e1a752014d61a54a88e13ca76ea1cd76
Eddy Varela <edv2@williams.edu>
2020-05-23 19:46:07 -0400

    connected to db

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require("express"),
-  app = express(),
-  port = process.env.PORT || 5000,
-  cors = require("cors");
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+
+const url = require("./config/db").uri;
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+ }
+).then(()=> {
+ console.log("Connection to DB established successfully");
+}).catch(err => console.log(err));
 
 app.use(cors());
 app.listen(port, () => console.log("Backend server live on " + port));
 
-app.get("/", (req, res) => {
-  res.send("Genesim Application");
-});
+// app.use('/api/forum', require('./routes/api/forumRoutes'));
